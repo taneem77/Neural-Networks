c8186aa4af144a1c47b00992b10c18c13de0bf54
Tanisha Mathur <mathurtanee07@gmail.com>
2026-07-30 21:58:56 +0530

    Improve comments and formatting in perceptron_kmod.h
    Updated comments for clarity and corrected formatting.

--- src/kernel/perceptron_kmod.h
+++ src/kernel/perceptron_kmod.h
@@ -1,12 +1,15 @@
+//its like whenever u communicate this is the format ull use so if kernel expects it in order of weights inputs and result thats what its going to give in
+
 #ifndef PERCEPTRON_KMOD_H
 #define PERCEPTRON_KMOD_H
 
 #include <linux/ioctl.h>
 
 /*
- * Maximum vector length supported.
+ * Maximum vector length supporte
  */
-#define MAX_VEC_LEN 64
+#define MAX_VEC_LEN 64 
+//maximum features supported 
 
 /*
  * Data exchanged between user space and kernel space.
@@ -19,7 +22,7 @@
  * Output:
  *      result    -> computed dot product
  */
-struct dot_product_request
+struct dot_product_request// this will be the package the moves from user space to kernel 
 {
     long inputs[MAX_VEC_LEN];
     long weights[MAX_VEC_LEN];
@@ -35,5 +38,6 @@ struct dot_product_request
 
 #define PERCEPTRON_DOT \
         _IOWR(PERCEPTRON_MAGIC, 1, struct dot_product_request)
+//ioctly command
 
 #endif
